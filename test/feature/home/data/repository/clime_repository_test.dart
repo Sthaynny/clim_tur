8c206ddd6caae75f0f4e1430b5c43995f1997c47
isthaynny <igor.nascimento@popcode.com.br>
2021-07-24 16:00:46 -0300

    feat: creating test

--- test/feature/home/data/repository/clime_repository_test.dart
+++ test/feature/home/data/repository/clime_repository_test.dart
@@ -1,7 +1,9 @@
 import 'package:clim_tur/app/feature/home/data/datasorce/clime_datasource.dart';
+import 'package:clim_tur/app/feature/home/data/datasorce/clime_local_datasource.dart';
 import 'package:clim_tur/app/feature/home/data/models/clime_model.dart';
 import 'package:clim_tur/app/feature/home/data/models/weathe_forecast_model.dart';
 import 'package:clim_tur/app/feature/home/data/repository/clime_repository.dart';
+import 'package:clim_tur/app/feature/home/domain/entity/clime_entity.dart';
 import 'package:clim_tur/app/feature/home/domain/repository/iclime_repository.dart';
 import 'package:clim_tur/core/network/dio/response/api_result.dart';
 import 'package:clim_tur/core/network/dio/response/states/apiError/api_error.dart';
@@ -25,12 +27,12 @@ final _tInstanceClimeModel = ClimeModel(
       tempMin: 45545,
     ));
 
-class _MockDataSource extends Mock implements ClimeDatasource {
+class _MockDataSourceSuccess extends Mock implements ClimeDatasource {
   @override
   Future<ApiResult> getClime(String search) async {
     return Success(
       statusCode: 200,
-      data: _tInstanceClimeModel.toJson(),
+      data: _tInstanceClimeModel.toMap(),
     );
   }
 }
@@ -44,24 +46,86 @@ class _MockDataSourceError extends Mock implements ClimeDatasource {
   }
 }
 
+class _MockLocalDataSourceSuccess extends Mock implements ClimeLocalDatasource {
+  @override
+  Future<ClimeEntity?> getClimeLocal() async {
+    return _tInstanceClimeModel;
+  }
+
+  @override
+  Future<bool> saveClimeLocal(ClimeEntity climeEntity) async {
+    return true;
+  }
+}
+
+class _MockLocalDataSourceError extends Mock implements ClimeLocalDatasource {
+  @override
+  Future<ClimeEntity?> getClimeLocal() async {
+    return null;
+  }
+
+  @override
+  Future<bool> saveClimeLocal(ClimeEntity climeEntity) async {
+    return false;
+  }
+}
+
 void main() {
-  IClimeRepository repository = ClimeRepository(datasource: _MockDataSource());
+  IClimeRepository repositorySuccess = ClimeRepository(
+    datasource: _MockDataSourceSuccess(),
+    datasourceLocal: _MockLocalDataSourceSuccess(),
+  );
+  IClimeRepository repositoryError = ClimeRepository(
+    datasource: _MockDataSourceError(),
+    datasourceLocal: _MockLocalDataSourceError(),
+  );
 
   group(
     'Resquest ClimeRepository',
     () {
       test('Success Request', () async {
-        final result = await repository.getClimes('test');
+        final result = await repositorySuccess.getClimes('test');
         expect(result.isRight(), true);
       });
 
       test('Error Request', () async {
-        repository = ClimeRepository(
-          datasource: _MockDataSourceError(),
-        );
-        final result = await repository.getClimes('test');
+        final result = await repositoryError.getClimes('test');
 
         expect(result.isLeft(), true);
+      });
+    },
+  );
+  group(
+    'Resquest Local',
+    () {
+      test('Success Request getClimesLocal', () async {
+        final result = await repositorySuccess.getClimesLocal();
+
+        expect(result != null, true);
+      });
+
+      test('Error Request getClimesLocal', () async {
+        final result = await repositoryError.getClimesLocal();
+
+        expect(result == null, true);
+      });
+    },
+  );
+  group(
+    'Resquest Save',
+    () {
+      test('Success Request saveClimesLocal', () async {
+        final result =
+            await repositorySuccess.saveClimesLocal(_tInstanceClimeModel);
+
+        expect(result, true);
+      });
+
+      test('Error Request saveClimesLocal', () async {
+        final result =
+            await repositoryError.saveClimesLocal(_tInstanceClimeModel);
+
+        expect(result, false);
       });
     },
   );
--- test/feature/home/presentation/home_cubit_test.dart
+++ test/feature/home/presentation/home_cubit_test.dart
@@ -1,1 +1,143 @@
-void main() {}
+import 'package:bloc_test/bloc_test.dart';
+import 'package:clim_tur/app/feature/home/data/models/clime_model.dart';
+import 'package:clim_tur/app/feature/home/data/models/weathe_forecast_model.dart';
+import 'package:clim_tur/app/feature/home/domain/entity/clime_entity.dart';
+import 'package:clim_tur/app/feature/home/domain/usercases/get_clime_local_usercase.dart';
+import 'package:clim_tur/app/feature/home/domain/usercases/get_clime_usercase.dart';
+import 'package:clim_tur/app/feature/home/domain/usercases/save_clime_local_usercase.dart';
+import 'package:clim_tur/app/feature/home/presentation/cubit/home_cubit.dart';
+import 'package:clim_tur/core/network/dio/response/states/apiError/api_error.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+final _tInstanceClimeModel = ClimeModel(
+    cod: 111,
+    id: 44,
+    name: 'asda',
+    weather: [],
+    weatherForecast: WeatherForecastModel(
+      temp: 115,
+      feelsLike: 15651,
+      grndLevel: 545,
+      humidity: 4554,
+      pressure: 4545,
+      seaLevel: 54545,
+      tempMax: 4554,
+      tempMin: 45545,
+    ));
+
+Future<void> main() async {
+  HomeCubit? homeCubitSuccess;
+  HomeCubit? homeCubitError;
+
+  setUp(() {
+    homeCubitSuccess = HomeCubit(
+        saveLocalClimeUsercase: MockSaveLocalUseCase(),
+        usercase: MockSearchUseCaseSuccess(),
+        localUsercase: MockSearchLocalUseCaseSuccess());
+    homeCubitError = HomeCubit(
+        saveLocalClimeUsercase: MockSaveLocalUseCase(),
+        usercase: MockSearchUseCaseError(),
+        localUsercase: MockSearchLocalUseCaseError());
+  });
+
+  tearDown(() {
+    homeCubitSuccess!.close();
+    homeCubitError!.close();
+  });
+
+  group('Home (cubit): ', () {
+    test(
+      'Verificar estado inicial',
+      () {
+        expect(
+          homeCubitSuccess!.state,
+          isA<HomeInitial>(),
+        );
+        expect(
+          homeCubitError!.state,
+          isA<HomeInitial>(),
+        );
+      },
+    );
+
+    blocTest<HomeCubit, HomeState>(
+      'get clime Success',
+      build: () {
+        return homeCubitSuccess!;
+      },
+      act: (bloc) async => bloc.loadClimeApi('asd'),
+      expect: () => [
+        isA<HomeLoading>(),
+        isA<HomeLoaded>(),
+      ],
+    );
+    blocTest<HomeCubit, HomeState>(
+      'get clime Error',
+      build: () {
+        return homeCubitError!;
+      },
+      act: (bloc) async => bloc.loadClimeApi('asd'),
+      expect: () => [
+        isA<HomeLoading>(),
+        isA<HomeLoadedError>(),
+      ],
+    );
+
+    blocTest<HomeCubit, HomeState>(
+      'get Local clime Success',
+      build: () {
+        return homeCubitSuccess!;
+      },
+      act: (bloc) async => bloc.loadClimeLocal(),
+      expect: () => [
+        isA<HomeLoading>(),
+        isA<HomeLoaded>(),
+      ],
+    );
+    blocTest<HomeCubit, HomeState>(
+      'get Local clime Error',
+      build: () {
+        return homeCubitError!;
+      },
+      act: (bloc) async => bloc.loadClimeLocal(),
+      expect: () => [
+        isA<HomeLoading>(),
+        isA<HomeLoadedLocalError>(),
+      ],
+    );
+  });
+}
+
+class MockSearchUseCaseSuccess extends Mock implements GetClimeUsercase {
+  Future<Either<ApiError, ClimeEntity>> call(String search) async {
+    return Right(_tInstanceClimeModel);
+  }
+}
+
+class MockSearchUseCaseError extends Mock implements GetClimeUsercase {
+  Future<Either<ApiError, ClimeEntity>> call(String search) async {
+    return Left(ApiError(statusCode: 404));
+  }
+}
+
+class MockSearchLocalUseCaseSuccess extends Mock
+    implements GetClimeLocalUsercase {
+  Future<ClimeEntity?> call() async {
+    return _tInstanceClimeModel;
+  }
+}
+
+class MockSearchLocalUseCaseError extends Mock
+    implements GetClimeLocalUsercase {
+  Future<ClimeEntity?> call() async {
+    return null;
+  }
+}
+
+class MockSaveLocalUseCase extends Mock implements SaveLocalClimeUsercase {
+  Future<bool> call(ClimeEntity climeEntity) async {
+    return true;
+  }
+}
